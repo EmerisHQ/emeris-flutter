1e617395720b12e6442919fa267e3e79e6132320
Andrzej Chmielewski <andrzejchm@users.noreply.github.com>
2021-07-08 20:59:21 +0200

    feat: Stick to exact versions of libraries (#15)

--- test/transaction_hosted_test.dart
+++ test/transaction_hosted_test.dart
@@ -8,90 +8,89 @@ import 'package:flutter_app/global.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  final api = CosmosApi();
-  final faucetApi = FaucetApi();
+  group("Transaction hosted test", () {
+    final api = CosmosApi();
+    final faucetApi = FaucetApi();
 
-  const port = '443';
-  const lcdUrl = 'api.testnet.cosmos.network';
+    const port = '443';
+    const lcdUrl = 'api.testnet.cosmos.network';
 
-  const ethUrl = 'https://ropsten.infura.io/v3/96ac5dcb92d545b6a7ffc3d8af21fde0';
+    const ethUrl = 'https://ropsten.infura.io/v3/96ac5dcb92d545b6a7ffc3d8af21fde0';
 
-  baseEnv.setEnv(lcdUrl, port, ethUrl);
+    baseEnv.setEnv(lcdUrl, port, ethUrl);
 
-  test('Import Alice wallet', () {
-    api.importWallet(
-        mnemonicString:
-            'brush bleak category link own under around element update either jungle trap base swamp fitness hour section skill soon bread cousin text evil jazz',
-        walletAlias: 'Alice');
-  });
+    test('Import Alice wallet', () {
+      api.importWallet(
+          mnemonicString:
+              'brush bleak category link own under around element update either jungle trap base swamp fitness hour section skill soon bread cousin text evil jazz',
+          walletAlias: 'Alice');
+    });
 
-  test('Import Bob wallet', () {
-    api.importWallet(
-        mnemonicString:
-            'oak favorite woman uniform try initial grant craft sun copper aim arctic grape mansion floor apology false crash hold family veteran under wink loan',
-        walletAlias: 'Bob');
-  });
+    test('Import Bob wallet', () {
+      api.importWallet(
+          mnemonicString:
+              'oak favorite woman uniform try initial grant craft sun copper aim arctic grape mansion floor apology false crash hold family veteran under wink loan',
+          walletAlias: 'Bob');
+    });
 
-  test('Get first wallet balances', () async {
-    final wallet = globalCache.wallets[0];
-    final address = wallet.walletDetails.walletAddress;
-    final balances = await api.getWalletBalances(address);
-    debugPrint(wallet.walletDetails.walletAlias);
-    for (final element in balances.list) {
-      debugPrint('${element.denom} ${element.amount}');
-    }
-  });
+    test('Get first wallet balances', () async {
+      final wallet = globalCache.wallets[0];
+      final address = wallet.walletDetails.walletAddress;
+      final balances = await api.getWalletBalances(address);
+      debugPrint(wallet.walletDetails.walletAlias);
+      for (final element in balances.list) {
+        debugPrint('${element.denom} ${element.amount}');
+      }
+    });
 
-  test('Get second wallet balances', () async {
-    final wallet = globalCache.wallets[1];
-    final address = wallet.walletDetails.walletAddress;
-    final balances = await api.getWalletBalances(address);
-    debugPrint(wallet.walletDetails.walletAlias);
+    test('Get second wallet balances', () async {
+      final wallet = globalCache.wallets[1];
+      final address = wallet.walletDetails.walletAddress;
+      final balances = await api.getWalletBalances(address);
+      debugPrint(wallet.walletDetails.walletAlias);
 
-    for (final element in balances.list) {
-      debugPrint('${element.denom} ${element.amount}');
-    }
-  });
+      for (final element in balances.list) {
+        debugPrint('${element.denom} ${element.amount}');
+      }
+    });
 
-  test('Get free tokens for Alice', () async {
-    final wallet = globalCache.wallets[0];
-    final address = wallet.walletDetails.walletAddress;
-    debugPrint('Getting free tokens for Alice');
-    await faucetApi.getFreeTokens(address);
-  });
+    test('Get free tokens for Alice', () async {
+      final wallet = globalCache.wallets[0];
+      final address = wallet.walletDetails.walletAddress;
+      debugPrint('Getting free tokens for Alice');
+      await faucetApi.getFreeTokens(address);
+    });
 
-  test('Get free tokens for Bob', () async {
-    final wallet = globalCache.wallets[1];
-    final address = wallet.walletDetails.walletAddress;
-    debugPrint('Getting free tokens for Bob');
-    await faucetApi.getFreeTokens(address);
-  });
+    test('Get free tokens for Bob', () async {
+      final wallet = globalCache.wallets[1];
+      final address = wallet.walletDetails.walletAddress;
+      debugPrint('Getting free tokens for Bob');
+      await faucetApi.getFreeTokens(address);
+    });
 
-  test('Get first wallet balances', () async {
-    final wallet = globalCache.wallets[0];
-    final address = wallet.walletDetails.walletAddress;
-    final balances = await api.getWalletBalances(address);
-    debugPrint(wallet.walletDetails.walletAlias);
+    test('Get first wallet balances', () async {
+      final wallet = globalCache.wallets[0];
+      final address = wallet.walletDetails.walletAddress;
+      final balances = await api.getWalletBalances(address);
+      debugPrint(wallet.walletDetails.walletAlias);
 
-    for (final element in balances.list) {
-      debugPrint('${element.denom} ${element.amount}');
-    }
-  });
+      for (final element in balances.list) {
+        debugPrint('${element.denom} ${element.amount}');
+      }
+    });
 
-  test('Get second wallet balances', () async {
-    final wallet = globalCache.wallets[1];
-    final address = wallet.walletDetails.walletAddress;
-    final balances = await api.getWalletBalances(address);
-    debugPrint(wallet.walletDetails.walletAlias);
+    test('Get second wallet balances', () async {
+      final wallet = globalCache.wallets[1];
+      final address = wallet.walletDetails.walletAddress;
+      final balances = await api.getWalletBalances(address);
+      debugPrint(wallet.walletDetails.walletAlias);
 
-    for (final element in balances.list) {
-      debugPrint('${element.denom} ${element.amount}');
-    }
-  });
+      for (final element in balances.list) {
+        debugPrint('${element.denom} ${element.amount}');
+      }
+    });
 
-  test(
-    'Make a transaction from Alice to Bob',
-    () async {
+    test('Make a transaction from Alice to Bob', () async {
       await api.sendAmount(
         fromAddress: globalCache.wallets[0].walletDetails.walletAddress,
         toAddress: globalCache.wallets[1].walletDetails.walletAddress,
@@ -100,28 +99,27 @@ void main() {
           denom: const Denom('uphoton'),
         ),
       );
-    },
-    skip: true,
-  );
+    }, skip: true);
 
-  test('Get first wallet balances', () async {
-    final wallet = globalCache.wallets[0];
-    final address = wallet.walletDetails.walletAddress;
-    final balances = await api.getWalletBalances(address);
-    debugPrint(wallet.walletDetails.walletAlias);
-    for (final element in balances.list) {
-      debugPrint('${element.denom} ${element.amount}');
-    }
-  });
+    test('Get first wallet balances', () async {
+      final wallet = globalCache.wallets[0];
+      final address = wallet.walletDetails.walletAddress;
+      final balances = await api.getWalletBalances(address);
+      debugPrint(wallet.walletDetails.walletAlias);
+      for (final element in balances.list) {
+        debugPrint('${element.denom} ${element.amount}');
+      }
+    });
 
-  test('Get second wallet balances', () async {
-    final wallet = globalCache.wallets[1];
-    final address = wallet.walletDetails.walletAddress;
-    final balances = await api.getWalletBalances(address);
-    debugPrint(wallet.walletDetails.walletAlias);
+    test('Get second wallet balances', () async {
+      final wallet = globalCache.wallets[1];
+      final address = wallet.walletDetails.walletAddress;
+      final balances = await api.getWalletBalances(address);
+      debugPrint(wallet.walletDetails.walletAlias);
 
-    for (final element in balances.list) {
-      debugPrint('${element.denom} ${element.amount}');
-    }
-  });
+      for (final element in balances.list) {
+        debugPrint('${element.denom} ${element.amount}');
+      }
+    });
+  }, skip: true);
 }
