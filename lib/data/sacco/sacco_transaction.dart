314589493c03f87cb19f7da6af17eeb988338b77
Andrzej Chmielewski <andrzejchm@gmail.com>
2021-07-23 11:51:29 +0300

    move sacco implementation to transaction_signing_gateway library

--- lib/data/sacco/sacco_transaction.dart
+++ lib/data/sacco/sacco_transaction.dart
@@ -2,24 +2,17 @@ import 'package:flutter_app/data/sacco/messages/sacco_send_money_message.dart';
 import 'package:flutter_app/domain/entities/send_money_message.dart';
 import 'package:flutter_app/domain/entities/transaction.dart';
 import 'package:sacco/sacco.dart' as sacco;
-import 'package:transaction_signing_gateway/model/signed_transaction.dart';
-import 'package:transaction_signing_gateway/model/unsigned_transaction.dart';
+import 'package:transaction_signing_gateway/transaction_signing_gateway.dart';
 
-class SaccoTransaction implements UnsignedTransaction, SignedTransaction {
-  final sacco.StdTx stdTx;
-
-  SaccoTransaction(this.stdTx);
-
-  static SaccoTransaction? fromDomain(Transaction transaction) {
-    switch (transaction.transactionType) {
-      case TransactionType.sendMoney:
-        final messages = transaction.messages
-            .whereType<SendMoneyMessage>()
-            .map(
-              (moneyMessage) => SaccoSendMoneyMessage.fromDomain(moneyMessage).toStdMsg(),
-            )
-            .toList();
-        return SaccoTransaction(sacco.TxBuilder.buildStdTx(stdMsgs: messages));
-    }
+SaccoTransaction? fromDomain(Transaction transaction) {
+  switch (transaction.transactionType) {
+    case TransactionType.sendMoney:
+      final messages = transaction.messages
+          .whereType<SendMoneyMessage>()
+          .map(
+            (moneyMessage) => SaccoSendMoneyMessage.fromDomain(moneyMessage).toStdMsg(),
+          )
+          .toList();
+      return SaccoTransaction(sacco.TxBuilder.buildStdTx(stdMsgs: messages));
   }
 }
