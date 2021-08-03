38ba4c9b50bb3694e282ecc63f8608f9e2c961ed
Waleed Arshad <25613623+wal33d006@users.noreply.github.com>
2021-08-03 13:26:57 +0500

    feat: Broadcasting through transaction signing gateway package (#29)
    * Update transaction hash class to be used from transaction_signing_gateway package. Use transaction broadcaster from gateway package.
    
    * Make the variable final in cosmos_wallet_api.dart

--- lib/data/emeris/emeris_transactions_repository.dart
+++ lib/data/emeris/emeris_transactions_repository.dart
@@ -2,9 +2,9 @@ import 'package:dartz/dartz.dart';
 import 'package:flutter_app/data/api_calls/wallet_api.dart';
 import 'package:flutter_app/domain/entities/failures/general_failure.dart';
 import 'package:flutter_app/domain/entities/transaction.dart';
-import 'package:flutter_app/domain/entities/transaction_hash.dart';
 import 'package:flutter_app/domain/entities/wallet_identifier.dart';
 import 'package:flutter_app/domain/repositories/transactions_repository.dart';
+import 'package:transaction_signing_gateway/model/transaction_hash.dart';
 
 class EmerisTransactionsRepository implements TransactionsRepository {
   final List<WalletApi> _walletApis;
