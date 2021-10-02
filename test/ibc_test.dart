f59921f78f224daba25f17ca16138362fd0ce7db
Andrzej Chmielewski <andrzejchm@gmail.com>
2021-10-02 06:40:08 +0200

    skip ibc tests

--- test/ibc_test.dart
+++ test/ibc_test.dart
@@ -3,36 +3,42 @@ import 'package:flutter_app/data/api_calls/ibc_api.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  final ibcApi = IbcApi(Dio());
+  group(
+    "IBC tests",
+    () {
+      final ibcApi = IbcApi(Dio());
 
-  test('Verify trace', () async {
-    const chainName = 'cosmos-hub';
-    const hash = '4129EB76C01ED14052054BB975DE0C6C5010E12FFD9253C20C58BCD828BEE9A5';
-    final response = await ibcApi.verifyTrace(chainName, hash);
-    response.fold(
-      (fail) => throw fail,
-      (json) => expect(json.trace.first.chainName, chainName),
-    );
-  });
+      test('Verify trace', () async {
+        const chainName = 'cosmos-hub';
+        const hash = '4129EB76C01ED14052054BB975DE0C6C5010E12FFD9253C20C58BCD828BEE9A5';
+        final response = await ibcApi.verifyTrace(chainName, hash);
+        response.fold(
+          (fail) => throw fail,
+          (json) => expect(json.trace.first.chainName, chainName),
+        );
+      });
 
-  test('Get verified denoms', () async {
-    final response = await ibcApi.getVerifiedDenoms();
-    response.fold(
-      (fail) => throw fail,
-      (json) => expect(json.isNotEmpty, true),
-    );
-  });
+      test('Get verified denoms', () async {
+        final response = await ibcApi.getVerifiedDenoms();
+        response.fold(
+          (fail) => throw fail,
+          (json) => expect(json.isNotEmpty, true),
+        );
+      });
 
-  test('Get primary channel', () async {
-    const chainName = 'cosmos-hub';
-    const destinationChainName = 'akash';
-    final response = await ibcApi.getPrimaryChannel(
-      chainId: chainName,
-      destinationChanName: destinationChainName,
-    );
-    response.fold(
-      (fail) => throw fail,
-      (json) => expect(json.counterParty, destinationChainName),
-    );
-  });
+      test('Get primary channel', () async {
+        const chainName = 'cosmos-hub';
+        const destinationChainName = 'akash';
+        final response = await ibcApi.getPrimaryChannel(
+          chainId: chainName,
+          destinationChanName: destinationChainName,
+        );
+        response.fold(
+          (fail) => throw fail,
+          (json) => expect(json.counterParty, destinationChainName),
+        );
+      });
+    },
+    skip: true,
+  );
 }
