1acf8a84f5e30fd8e6c9210b98cdf1d5ddb36590
Andrzej Chmielewski <andrzejchm@gmail.com>
2021-10-15 11:15:51 +0200

    fix routing navigation

--- lib/presentation/routing/routing_presenter.dart
+++ lib/presentation/routing/routing_presenter.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_app/presentation/routing/routing_presentation_model.dart';
+import 'package:flutter_app/presentation/wallets_list/wallets_list_initial_params.dart';
 import 'package:flutter_app/ui/pages/onboarding/onboarding_initial_params.dart';
 import 'package:flutter_app/ui/pages/routing/routing_navigator.dart';
 import 'package:flutter_app/utils/app_initializer.dart';
@@ -21,10 +22,10 @@ class RoutingPresenter {
       await _appInitializer.init();
     }
     navigator.close();
-    // if (_model.wallets.isEmpty) {
-    navigator.openOnboarding(const OnboardingInitialParams());
-    // } else {
-    //   navigator.openWalletsList(const WalletsListInitialParams());
-    // }
+    if (_model.wallets.isEmpty) {
+      navigator.openOnboarding(const OnboardingInitialParams());
+    } else {
+      navigator.openWalletsList(const WalletsListInitialParams());
+    }
   }
 }
