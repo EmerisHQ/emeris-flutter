61869b53b996cd5fd14d020c3fa7533d8126bd61
wal33d006 <waleed_arshad006@hotmail.com>
2021-10-26 15:00:25 +0500

    Remove optional parameters from get_ethereum_balances.dart

--- lib/data/ethereum/actions/get_ethereum_balances.dart
+++ lib/data/ethereum/actions/get_ethereum_balances.dart
@@ -23,8 +23,6 @@ Future<Either<GeneralFailure, PaginatedList<Balance>>> getEthereumBalances(
           Balance(
             denom: const Denom('ETH'),
             amount: Amount.fromString(balance.getValueInUnit(EtherUnit.ether).toString()),
-            unitPrice: Amount.fromString('0'),
-            dollarPrice: Amount.fromString('0'),
           ),
         ],
         pagination: EthereumPagination(),
