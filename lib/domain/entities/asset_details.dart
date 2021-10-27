ce1e8df3a94d0d93aed8ca7540f98ef0711b3f4d
wal33d006 <waleed_arshad006@hotmail.com>
2021-10-27 13:21:31 +0500

    Review PR suggestion

--- lib/domain/entities/asset_details.dart
+++ lib/domain/entities/asset_details.dart
@@ -5,18 +5,10 @@ import 'package:flutter_app/domain/entities/balance.dart';
 class AssetDetails extends Equatable {
   final List<Balance> balances;
 
-  Amount get totalAmountInUSD => Amount.fromString(_calculateTotalAmount().toStringAsFixed(2));
+  Amount get totalAmountInUSD => Amount(balances.map((it) => it.dollarPrice.value).reduce((a, b) => a + b));
 
   const AssetDetails({required this.balances});
 
   @override
   List<Object?> get props => [balances, totalAmountInUSD];
-
-  double _calculateTotalAmount() {
-    var totalAmount = 0.0;
-    for (final element in balances) {
-      totalAmount += element.dollarPrice.value.toDouble();
-    }
-    return totalAmount;
-  }
 }
