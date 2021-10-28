4548ea24143cff69f934d50dcd4b9e930caa15ce
wal33d006 <waleed_arshad006@hotmail.com>
2021-10-28 20:01:44 +0500

    Remove inheritance from verified_denom_json.dart

--- lib/data/model/verified_denom_json.dart
+++ lib/data/model/verified_denom_json.dart
@@ -1,42 +1,52 @@
-import 'package:flutter_app/data/model/denom_json.dart';
 import 'package:flutter_app/data/model/gas_price_levels_json.dart';
 import 'package:flutter_app/domain/entities/verified_denom.dart';
 
-class VerifiedDenomJson extends DenomJson {
+class VerifiedDenomJson {
   late String chainName;
+  final String name;
+  final String displayName;
+  final String logo;
+  final int precision;
+  final bool verified;
+  final bool stakable;
+  final String ticker;
+  final bool feeToken;
+  final GasPriceLevelsJson gasPriceLevels;
+  final bool fetchPrice;
+  final bool relayerDenom;
+  final int minimumThreshRelayerBalance;
 
   VerifiedDenomJson({
-    required String name,
-    required String displayName,
-    required String logo,
-    required int precision,
-    required bool verified,
-    required bool stakable,
-    required String ticker,
-    required bool feeToken,
-    required GasPriceLevelsJson gasPriceLevels,
-    required bool fetchPrice,
-    required bool relayerDenom,
-    required int minimumThreshRelayerBalance,
+    required this.name,
+    required this.displayName,
+    required this.logo,
+    required this.precision,
+    required this.verified,
+    required this.stakable,
+    required this.ticker,
+    required this.feeToken,
+    required this.gasPriceLevels,
+    required this.fetchPrice,
+    required this.relayerDenom,
+    required this.minimumThreshRelayerBalance,
     required this.chainName,
-  }) : super(
-          name: name,
-          displayName: displayName,
-          feeToken: feeToken,
-          fetchPrice: fetchPrice,
-          gasPriceLevels: gasPriceLevels,
-          logo: logo,
-          minimumThreshRelayerBalance: minimumThreshRelayerBalance,
-          precision: precision,
-          relayerDenom: relayerDenom,
-          stakable: stakable,
-          ticker: ticker,
-          verified: verified,
-        );
+  });
 
-  VerifiedDenomJson.fromJson(Map<String, dynamic> json) : super.fromJson(json) {
-    chainName = json['chain_name'] as String;
-  }
+  factory VerifiedDenomJson.fromJson(Map<String, dynamic> json) => VerifiedDenomJson(
+        name: json['name'] as String? ?? '',
+        displayName: json['display_name'] as String? ?? '',
+        logo: json['logo'] as String? ?? '',
+        precision: json['precision'] as int? ?? -1,
+        verified: json['verified'] as bool? ?? false,
+        stakable: json['stakable'] as bool? ?? false,
+        ticker: json['ticker'] as String? ?? '',
+        feeToken: json['fee_token'] as bool? ?? false,
+        gasPriceLevels: GasPriceLevelsJson.fromJson(json['gas_price_levels'] as Map<String, dynamic>),
+        fetchPrice: json['fetch_price'] as bool? ?? false,
+        relayerDenom: json['relayer_denom'] as bool? ?? false,
+        minimumThreshRelayerBalance: json['minimum_thresh_relayer_balance'] as int? ?? -1,
+        chainName: json['chain_name'] as String? ?? '',
+      );
 
   VerifiedDenom toDomain() => VerifiedDenom(
         chainName: chainName,
