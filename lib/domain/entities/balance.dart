52f0cca60ff9934769cbb36f0e5381f48d38769d
Waleed Arshad <25613623+wal33d006@users.noreply.github.com>
2022-02-18 14:59:51 +0500

    fix: Balance's Amount does not take into account the precision of given token (#111)
    * Add precision to amount
    
    * Refactor code
    
    Co-authored-by: Andrzej Chmielewski <andrzejchm@users.noreply.github.com>

--- lib/domain/entities/balance.dart
+++ lib/domain/entities/balance.dart
@@ -58,7 +58,7 @@ class Balance extends Equatable {
       ];
 
   Balance byUpdatingPriceAndVerifiedDenom(Price price, List<VerifiedDenom> verifiedDenoms) {
-    final baseDenomDisplayText = verifiedDenoms.firstWhere((element) => element.name == denom.text).displayName;
+    final baseDenom = verifiedDenoms.firstWhere((element) => element.name == denom.text);
     final ticker = '${verifiedDenoms.firstWhere((element) => element.name == denom.text).ticker}USDT';
 
     /// TODO: Pick up the pool token prices from the API or calculate it as done in the web
@@ -67,8 +67,8 @@ class Balance extends Equatable {
     final dollarPrice = amount.value * unitPrice;
 
     return Balance(
-      denom: Denom(baseDenomDisplayText),
-      amount: amount,
+      denom: Denom(baseDenom.displayName),
+      amount: Amount(amount.value / Decimal.fromInt(10).pow(baseDenom.precision)),
       unitPrice: Amount.fromString(unitPrice.toStringAsFixed(2)),
       dollarPrice: Amount.fromString(dollarPrice.toStringAsFixed(2)),
       onChain: onChain,
