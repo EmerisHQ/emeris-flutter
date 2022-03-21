a5c2c6d98b7d6e7b8643e9d1eb9899f4b22c03ca
Andrzej Chmielewski <andrzejchm@gmail.com>
2022-03-21 13:20:26 +0100

    REVERT IT: remove too many constructor arguments

--- lib/dependency_injection/app_component.dart
+++ lib/dependency_injection/app_component.dart
@@ -406,13 +406,11 @@ void _configureMvp() {
         _params,
         getIt(),
         getIt(),
-        getIt(),
       ),
     )
     ..registerFactoryParam<SendTokensPresenter, SendTokensInitialParams, dynamic>(
       (initialParams, _) => SendTokensPresenter(
         getIt(param1: initialParams),
-        getIt(),
         getIt(),
         getIt(),
       ),
