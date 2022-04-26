7d8286d9c4f33eced713a905f02b1fbd024e079a
waleedarshad <waleed_arshad006@hotmail.com>
2022-04-26 10:43:52 +0500

    Update MVP architecture, update design for account_backup_intro_page.dart

--- lib/ui/pages/account_backup/account_backup_intro/account_backup_intro_page.dart
+++ lib/ui/pages/account_backup/account_backup_intro/account_backup_intro_page.dart
@@ -1,24 +1,20 @@
+import 'package:cosmos_ui_components/cosmos_text_theme.dart';
 import 'package:cosmos_ui_components/cosmos_ui_components.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_app/dependency_injection/app_component.dart';
-import 'package:flutter_app/ui/pages/account_backup/account_backup_intro/account_backup_intro_initial_params.dart';
-import 'package:flutter_app/ui/pages/account_backup/account_backup_intro/account_backup_intro_navigator.dart';
+import 'package:flutter_app/generated_assets/assets.gen.dart';
 import 'package:flutter_app/ui/pages/account_backup/account_backup_intro/account_backup_intro_presentation_model.dart';
 import 'package:flutter_app/ui/pages/account_backup/account_backup_intro/account_backup_intro_presenter.dart';
 import 'package:flutter_app/ui/widgets/bullet_point_message.dart';
-import 'package:flutter_app/ui/widgets/emeris_logo_app_bar.dart';
 import 'package:flutter_app/utils/strings.dart';
 
 class AccountBackupIntroPage extends StatefulWidget {
   const AccountBackupIntroPage({
-    required this.initialParams,
+    required this.presenter,
     Key? key,
-    this.presenter, // useful for tests
   }) : super(key: key);
 
-  final AccountBackupIntroInitialParams initialParams;
-  final AccountBackupIntroPresenter? presenter;
+  final AccountBackupIntroPresenter presenter;
 
   @override
   AccountBackupIntroPageState createState() => AccountBackupIntroPageState();
@@ -26,25 +22,18 @@ class AccountBackupIntroPage extends StatefulWidget {
   @override
   void debugFillProperties(DiagnosticPropertiesBuilder properties) {
     super.debugFillProperties(properties);
-    properties
-      ..add(DiagnosticsProperty<AccountBackupIntroInitialParams>('initialParams', initialParams))
-      ..add(DiagnosticsProperty<AccountBackupIntroPresenter?>('presenter', presenter));
+    properties.add(DiagnosticsProperty<AccountBackupIntroPresenter?>('presenter', presenter));
   }
 }
 
 class AccountBackupIntroPageState extends State<AccountBackupIntroPage> {
-  late AccountBackupIntroPresenter presenter;
+  AccountBackupIntroPresenter get presenter => widget.presenter;
 
   AccountBackupIntroViewModel get model => presenter.viewModel;
 
   @override
   void initState() {
     super.initState();
-    presenter = widget.presenter ??
-        getIt(
-          param1: AccountBackupIntroPresentationModel(widget.initialParams, getIt()),
-          param2: getIt<AccountBackupIntroNavigator>(),
-        );
     presenter.navigator.context = context;
   }
 
@@ -52,56 +41,65 @@ class AccountBackupIntroPageState extends State<AccountBackupIntroPage> {
   Widget build(BuildContext context) {
     final theme = CosmosTheme.of(context);
     return Scaffold(
-      appBar: const EmerisLogoAppBar(),
+      appBar: const CosmosAppBar(
+        leading: CosmosBackButton(text: ''),
+      ),
       body: SafeArea(
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: theme.spacingL),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Text(
-                strings.backUpYourAccountTitle,
-                textAlign: TextAlign.center,
-                style: Theme.of(context).textTheme.headline4,
-              ),
-              SizedBox(height: theme.spacingM),
-              Text(
-                strings.backUpYourAccountMessage,
-                textAlign: TextAlign.center,
-              ),
-              const Spacer(),
-              BulletPointMessage(
-                message: strings.backUpAccountInfoMessage1,
-              ),
-              SizedBox(height: theme.spacingL),
-              BulletPointMessage(
-                message: strings.backUpAccountInfoMessage2,
-              ),
-              SizedBox(height: theme.spacingL),
-              BulletPointMessage(
-                message: strings.backUpAccountInfoMessage3,
-              ),
-              SizedBox(height: theme.spacingM),
-              const Spacer(),
-              CosmosElevatedButton(
-                text: model.isIcloudAvailable ? strings.backUpIcloudAction : strings.backUpGoogleDriveAction,
-                onTap: presenter.onTapCloudBackup,
-              ),
-              SizedBox(
-                height: CosmosTheme.of(context).spacingM,
-              ),
-              CosmosElevatedButton(
-                text: strings.backUpManuallyAction,
-                onTap: presenter.onTapManualBackup,
-              ),
-              SizedBox(
-                height: CosmosTheme.of(context).spacingM,
-              ),
-              CosmosTextButton(
-                text: strings.backUpLaterAction,
-                onTap: presenter.onTapBackupLater,
-              ),
-            ],
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(Assets.imagesBackupBackground.path),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: theme.spacingL),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text(
+                  strings.backUpYourAccountTitle,
+                  style: CosmosTextTheme.title2Bold,
+                ),
+                SizedBox(height: theme.spacingM),
+                Text(
+                  strings.backUpYourAccountMessage,
+                  style: CosmosTextTheme.copy0Normal,
+                ),
+                const Spacer(),
+                BulletPointMessage(
+                  message: strings.backUpAccountInfoMessage1,
+                ),
+                SizedBox(height: theme.spacingL),
+                BulletPointMessage(
+                  message: strings.backUpAccountInfoMessage2,
+                ),
+                SizedBox(height: theme.spacingL),
+                BulletPointMessage(
+                  message: strings.backUpAccountInfoMessage3,
+                ),
+                SizedBox(height: theme.spacingM),
+                const Spacer(),
+                CosmosElevatedButton(
+                  text: model.isIcloudAvailable ? strings.backUpIcloudAction : strings.backUpGoogleDriveAction,
+                  onTap: presenter.onTapCloudBackup,
+                ),
+                SizedBox(
+                  height: CosmosTheme.of(context).spacingM,
+                ),
+                CosmosElevatedButton(
+                  text: strings.backUpManuallyAction,
+                  onTap: presenter.onTapManualBackup,
+                ),
+                SizedBox(
+                  height: CosmosTheme.of(context).spacingM,
+                ),
+                CosmosTextButton(
+                  text: strings.backUpLaterAction,
+                  onTap: presenter.onTapBackupLater,
+                ),
+              ],
+            ),
           ),
         ),
       ),
