f2a74482664bff9876017d79f5ebcfa33e11a1d1
waleedarshad <waleed_arshad006@hotmail.com>
2022-05-18 15:03:51 +0500

    Update theme changes according to the updates in `cosmos_ui_components`

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,4 +1,3 @@
-import 'package:cosmos_ui_components/cosmos_app_theme.dart';
 import 'package:cosmos_ui_components/cosmos_text_theme.dart';
 import 'package:cosmos_ui_components/cosmos_theme.dart';
 import 'package:flutter/material.dart';
@@ -9,38 +8,35 @@ import 'package:flutter_app/ui/pages/routing/routing_page.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
-const darkThemeData = CosmosThemeData(
-  colors: darkThemeColors,
-  brightness: Brightness.dark,
-);
-
 class AppWidget extends StatelessWidget {
   const AppWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return CosmosTheme(
-      // themeData: darkThemeData,
-      child: Builder(builder: (context) {
-        return MaterialApp(
-          debugShowCheckedModeBanner: false,
-          title: 'Flutter Demo',
-          theme: CosmosTheme.buildTheme(context),
-          home: const RoutingPage(
-            initialParams: RoutingInitialParams(
-              initializeApp: true,
+      brightness: Brightness.dark,
+      child: Builder(
+        builder: (context) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'Flutter Demo',
+            theme: CosmosTheme.of(context).buildFlutterTheme(),
+            home: const RoutingPage(
+              initialParams: RoutingInitialParams(
+                initializeApp: true,
+              ),
             ),
-          ),
-          navigatorKey: AppNavigator.navigatorKey,
-          localizationsDelegates: const [
-            AppLocalizations.delegate,
-            GlobalMaterialLocalizations.delegate,
-            GlobalWidgetsLocalizations.delegate,
-            GlobalCupertinoLocalizations.delegate,
-          ],
-          supportedLocales: const [Locale('en', '')],
-        );
-      }),
+            navigatorKey: AppNavigator.navigatorKey,
+            localizationsDelegates: const [
+              AppLocalizations.delegate,
+              GlobalMaterialLocalizations.delegate,
+              GlobalWidgetsLocalizations.delegate,
+              GlobalCupertinoLocalizations.delegate,
+            ],
+            supportedLocales: const [Locale('en', '')],
+          );
+        },
+      ),
     );
   }
 
